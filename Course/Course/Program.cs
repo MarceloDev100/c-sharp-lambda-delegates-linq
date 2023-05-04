adb5d8f9b0a498f62675509a2fc93c97ff6a5e0e
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-05-04 20:43:56 -0300

    Small example using LINQ

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,35 +1,24 @@
-﻿using Course.Entities;
-
-namespace Course
+﻿namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Product> list = new List<Product>();
+            // LINQ
 
-            list.Add(new Product("Tv", 900.00));
-            list.Add(new Product("Mouse", 50.00));
-            list.Add(new Product("Tablet", 350.50));
-            list.Add(new Product("HD Case", 80.90));
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
 
+            // Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
 
-            /*
-              Write a program that, from a products list, 
-              generates a new list containing the names of products in uppercase.
-             */
-
-            // Func (System)
-            // - Represents a delegate
-            // - It's a method that returns a value and receives zero or more arguments.
-
-           
-            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
-
-            foreach (string p in result)
+            // Execute the query
+            foreach(int x in result)
             {
-                Console.WriteLine(p);
-            }
+                Console.WriteLine(x);
+            }   
         }
     }
 }
