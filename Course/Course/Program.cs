54b4845df26dba0452d708708210f1fccb7516a5
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-05-04 20:27:19 -0300

    Func - Inline lambda expression

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -23,9 +23,8 @@ namespace Course
             // - Represents a delegate
             // - It's a method that returns a value and receives zero or more arguments.
 
-            Func<Product, string> func = p => p.Name.ToUpper();
-
-            List<string> result = list.Select(func).ToList();
+           
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
 
             foreach (string p in result)
             {
