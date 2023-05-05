5016f903074147554551bceaf8e5b4ced742dd0e
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-05-04 23:18:30 -0300

    LINQ - Similar notation to SQL

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Course.Entities;
+using System.Security.Cryptography;
 
 namespace Course
 {
@@ -7,7 +8,7 @@ namespace Course
         static void Print<T>(string message, IEnumerable<T> collection)
         {
             Console.WriteLine(message);
-            foreach(T obj in collection)
+            foreach (T obj in collection)
             {
                 Console.WriteLine(obj);
             }
@@ -37,61 +38,71 @@ namespace Course
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
             Print("TIER 1 AND PRICE < 900: ", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAME OF PRODUCTS FROM TOOLS: ", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name , p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name , p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
+
             Print("NAME STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p)
+                 .Skip(2)
+                 .Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME, SKIP 2 TAKE 4 ", r5);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 = (from p in products select p).FirstOrDefault();
             Console.WriteLine("First or default test1: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            var r7 =
+                (from p in products
+                 where p.Price > 3000
+                 select p)
+                 .FirstOrDefault();
             Console.WriteLine("First or default test2: " + r7);
             Console.WriteLine();
 
-            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-            Console.WriteLine("Single or default test 1: " + r8);
-
-            var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-            Console.WriteLine("Single or default test 2: " + r9);
-
-            var r10 = products.Max(p => p.Price);
-            Console.WriteLine("Max price: " + r10); 
-            
-            var r11 = products.Min(p => p.Price);
-            Console.WriteLine("Min price: " + r11);
-
-            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-            Console.WriteLine("Category 1 prices sum: " + r12);
-
-            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-            Console.WriteLine("Category 1 prices average: " + r13);
-
-            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Category 5 prices average: " + r14);
-
-            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((x, y) => x + y);
-            Console.WriteLine("Category 1 aggregate sum: " + r15);
-
-            var r16 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-            Console.WriteLine("Category 5 aggregate sum: " + r16);
-            Console.WriteLine();
-
-            var r17 = products.GroupBy(p => p.Category);
-            foreach(IGrouping<Category, Product> group in r17)
+            //var r17 = products.GroupBy(p => p.Category);
+            var r17 =
+                from p in products
+                group p by p.Category;
+            foreach (IGrouping<Category, Product> group in r17)
             {
                 Console.WriteLine("Category " + group.Key.Name + ": ");
-                foreach(Product p in group)
+                foreach (Product p in group)
                 {
                     Console.WriteLine(p);
                 }
