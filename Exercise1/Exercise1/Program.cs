394087df62dee54b831092def5e7a966ae7686a3
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-05-05 14:05:30 -0300

    Exercise 1 - LINQ with lambda expressions

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Exercise1.Entities;
+using System.Globalization;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+
+                        Product prod = new Product(name, price);
+                        list.Add(prod);
+                    }
+                }
+
+                double average = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine("Average price: " + average.ToString("F2", CultureInfo.InvariantCulture));
+
+                var names = list.Where(p => p.Price < average).OrderByDescending(p => p.Name).Select(p => p.Name);
+                
+                foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Exercise1/README.md
@@ -0,0 +1,2 @@
+![Slide1](https://user-images.githubusercontent.com/88597534/236520682-7dddea67-208a-470b-b482-454ad1eb47e7.jpg)
+
